c394e24e5d823fb4c298914d920e55a8c814bb8f
DriAzz <driazz106@gmail.com>
2019-09-26 15:12:02 -0600

    Auth

---
+++ server-express-mysql/models/users.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Password: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {});
+  users.associate = function (models) {
+    // associations can be defined here
+  };
+  return users;
+};
